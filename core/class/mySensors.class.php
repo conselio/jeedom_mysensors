5f8173b0f72fddcc484da77b25e2263e31e42283
Cédric Dupont <lunarok@gmail.com>
2014-10-30 15:16:34 +0100

    Bad id of config key

--- core/class/mySensors.class.php
+++ core/class/mySensors.class.php
@@ -289,7 +289,7 @@ class mySensors extends eqLogic {
 	
 	public static function sendToController( $destination, $sensor, $command, $acknowledge, $type, $payload ) {
 		if (config::byKey('externalDeamon', 'mySensors', 0) == 2) {
-			$jeeSlave = jeeNetwork::byId(config::byKey('esclave', 'mySensors'));
+			$jeeSlave = jeeNetwork::byId(config::byKey('jeeSlave', 'mySensors'));
 			$urlNode = getIpFromString($jeeNetwork->getIp());
 		} else {
 			$urlNode = "127.0.0.1";
