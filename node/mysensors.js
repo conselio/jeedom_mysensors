7fd22dc54a4bb844850966bf01e2dcf5b6750a2e
Cédric Dupont <lunarok@gmail.com>
2014-11-08 23:52:54 +0100

    Add LibVersion record

--- node/mysensors.js
+++ node/mysensors.js
@@ -243,6 +243,19 @@ function saveSketchVersion(sender, payload ) {
 	});
 }
 
+function saveLibVersion(sender, payload ) {
+
+	console.log(Date() + " | info | Save saveLibVersion : " + "Value-" + sender.toString() + "-" + payload );
+
+		url = urlJeedom + "&messagetype=saveLibVersion&type=mySensors&id="+sender.toString()+"&value="+payload;
+
+		request(url, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+		console.log(Date() + " | info | Got response saveSketchVersion: " + response.statusCode);
+	  }
+	});
+}
+
 function sendTime(destination, sensor, gw) {
 	var payload = new Date().getTime();
  	var command = C_INTERNAL;
@@ -324,6 +337,7 @@ function rfReceived(data, db, gw) {
 				;
 			else
 				saveSensor(sender, sensor, type);
+				saveLibVersion(sender, payload);
 			break;
 		case C_SET:
 			saveValue(sender, sensor, type, payload);
