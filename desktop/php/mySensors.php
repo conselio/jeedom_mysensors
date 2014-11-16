147f7f125ec30a3afb10f69243058c24f00149ef
Cédric Dupont <lunarok@gmail.com>
2014-11-16 23:26:28 +0100

    Update mySensors.php

--- desktop/php/mySensors.php
+++ desktop/php/mySensors.php
@@ -124,6 +124,7 @@ sendVarToJS('mySensorDico', mySensors::$_dico);
         </form>
 
 	<legend>{{mySensors}}</legend>
+	<a class="btn btn-default btn-sm" id="bt_restartEq"><i class="fa fa-poweroff"></i> {{Reboot du Node}}</a>
         <a class="btn btn-default btn-sm" id="bt_addmySensorsInfo"><i class="fa fa-plus-circle"></i> {{Ajouter une info}}</a>
         <a class="btn btn-default btn-sm" id="bt_addmySensorsAction"><i class="fa fa-plus-circle"></i> {{Ajouter une commande}}</a><br/><br/>
         		<script>
