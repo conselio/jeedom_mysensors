58a320c15bfd2ee773522ef474a65c19ae8491c1
Cédric Dupont <lunarok@gmail.com>
2014-11-27 22:24:13 +0100

    Update mySensors.js

--- desktop/js/mySensors.js
+++ desktop/js/mySensors.js
@@ -79,11 +79,8 @@ function addCmdToTable(_cmd) {
 	tr += '<textarea class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="value" style="height : 33px;" ' + disabled + ' placeholder="{{Valeur}}" readonly=true></textarea>';		
         tr += '</td>';
         tr += '<td><input class="cmdAttr form-control input-sm" data-l1key="unite" style="width : 90px;" placeholder="{{Unite}}">';
-        	tr += '<span>{{Type de donnée}}:<select class="cmdAttr" data-l1key="configuration" data-l2key="cmdtype">';
-			$.each(mySensorDico['A'],function(item, index){ 
-				tr += '<option value="' + index + '">' + item + '</option>';
-			})
-		tr +='</select></span>';
+	tr += 'Valeur actuelle :<br/>';		
+	tr += '<textarea class="cmdAttr form-control input-sm" data-l1key="configuration" data-l2key="sensorType" style="height : 33px;" ' + disabled + ' placeholder="{{Type de donnée}}" readonly=true></textarea>';		
         tr += '</td><td>';
         tr += '<span><input type="checkbox" class="cmdAttr" data-l1key="isHistorized"/> {{Historiser}}<br/></span>';
         tr += '<span><input type="checkbox" class="cmdAttr" data-l1key="isVisible" checked/> {{Afficher}}<br/></span>';
