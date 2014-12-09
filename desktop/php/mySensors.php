cca7fe5d4ced99f9a251e1d012d537aee2ce7a1d
Cédric Dupont <lunarok@gmail.com>
2014-12-09 22:13:54 +0100

    Update mySensors.php

--- desktop/php/mySensors.php
+++ desktop/php/mySensors.php
@@ -33,12 +33,37 @@ sendVarToJS('mySensorDico', mySensors::$_dico);
             </ul>
         </div>
     </div>
+    
+    <div class="col-lg-10 col-md-9 col-sm-8 eqLogicThumbnailDisplay" style="border-left: solid 1px #EEE; padding-left: 25px;">
+        <legend>{{Mes virtuels}}
+        </legend>
+        <?php
+        if (count($eqLogics) == 0) {
+            echo "<br/><br/><br/><center><span style='color:#767676;font-size:1.2em;font-weight: bold;'>{{Vous n'avez pas encore de virtuel, cliquez sur Ajouter un équipement pour commencer}}</span></center>";
+        } else {
+            ?>
+            <div class="eqLogicThumbnailContainer">
+                <?php
+                foreach ($eqLogics as $eqLogic) {
+                    echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
+                    echo "<center>";
+                    echo '<img src="plugins/virtual/doc/images/virtual_icon.png" height="105" width="95" />';
+                    echo "</center>";
+                    echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
+                    echo '</div>';
+                }
+                ?>
+            </div>
+        <?php } ?>
+    </div>    
+    
+    
     <div class="col-lg-10 col-md-9 col-sm-8 eqLogic" style="border-left: solid 1px #EEE; padding-left: 25px;display: none;">
         <div class="row">
             <div class="col-sm-6">
                 <form class="form-horizontal">
             <fieldset>
-                <legend>{{Général}}</legend>
+                <legend><i class="fa fa-arrow-circle-left eqLogicAction cursor" data-action="returnToThumbnailDisplay"></i>{{Général}}</legend>
                 <div class="form-group">
                     <label class="col-md-2 control-label">{{Nom du Node}}</label>
                     <div class="col-md-3">
