88a1ecfc4980a6d62b64d8d49970be1d0c878794
Cédric Dupont <lunarok@gmail.com>
2014-12-14 16:41:09 +0100

    Update mySensors.class.php

--- core/class/mySensors.class.php
+++ core/class/mySensors.class.php
@@ -234,6 +234,7 @@ class mySensors extends eqLogic {
 		}
 	
 	$inclusion = config::byKey('include_mode', 'mySensors');
+	$inclusion = 'on';
 	$sensor_path = realpath(dirname(__FILE__) . '/../../node');	
         $cmd = 'nice -n 19 node ' . $sensor_path . '/mysensors.js ' . $url . ' ' . $usbGateway . ' ' . $gateMode . ' ' . $gatePort . ' ' . $inclusion;
 		
