19d374659ad388eb7cfaf66660b91ddd6601fb8b
Cédric Dupont <lunarok@gmail.com>
2014-12-14 15:12:37 +0100

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -74,11 +74,13 @@ echo '<option value="' . $jeeNetwork->getId(). '">' . $jeeNetwork->getName() . '
 			<div class="col-lg-2">
 			<input type="checkbox" data-l1key="include_mode" checked/>
 			</div>
+	</div>
+	<div>
 		<label class="col-lg-4 control-label">Redémarrage</label>
 			<div class="col-lg-2">
 			<a class="btn btn-warning" id="bt_restartmySensors"><i class='fa fa-stop'></i> Arrêter/Redemarrer le service NodeJS</a>
 			</div>
-		</div>		
+	</div>		
  <?php
  if (!mySensors::deamonRunning()) {
 echo '<div id="div_node" class="alert alert-danger">Le service NodeJS ne tourne pas</div>';
